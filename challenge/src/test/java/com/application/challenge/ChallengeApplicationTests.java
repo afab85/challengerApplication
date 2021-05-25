56e1e58581fedddef8645c8e8d1f272a5d883273
André Felipe de Almeida Batista <andre.felipe.1985@gmail.com>
2021-05-25 19:05:09 -0300

    unit test for music type product
    new dependency and a unit test class for when the producer is of the music type

--- challenge/src/test/java/com/application/challenge/ChallengeApplicationTests.java
+++ challenge/src/test/java/com/application/challenge/ChallengeApplicationTests.java
@@ -1,25 +1,62 @@
 package com.application.challenge;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
-import com.application.controller.PurchaseOrderController;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.application.models.PurchaseOrder;
 
 
-@WebMvcTest
+
 class ChallengeApplicationTests {
-	
-	private PurchaseOrderController purchaseOrderController;
-	
-	@BeforeEach
-	public void setup() {
+
+	@Test
+	public void mustCalculateTax_WhenProductIsMusicType() {
+		PurchaseOrder po = new PurchaseOrder();
+		
+		po.setTextOrder("1 music CD at 14.99");
+		
+		assertEquals(3.75, po.salesTaxCalculator(po));
+		assertEquals(true, po.isMusicProduct());
+		assertEquals(false, po.isImport());
+		assertEquals(false, po.isBasicTaxFree());
 		
 	}
 	
 	@Test
-	public void contextLoads() {
+	public void doNotCalculateTax_WhenProductIsNotMusicType() {
+		PurchaseOrder po = new PurchaseOrder();
 		
+		po.setTextOrder("1 apple at 00.50");
+		
+		assertEquals(0.00, po.salesTaxCalculator(po));
+		assertEquals(false, po.isMusicProduct());
+		assertEquals(false, po.isImport());
+		assertEquals(true, po.isBasicTaxFree());
+	}
+	
+	@Test
+	public void mustCalculateTax_WhenProductIsImportMusicType() {
+		PurchaseOrder po = new PurchaseOrder();
+		
+		po.setTextOrder("2 imported music CD at 10.00");
+		
+		assertEquals(6.00, po.salesTaxCalculator(po));
+		assertEquals(true, po.isMusicProduct());
+		assertEquals(true, po.isImport());
+		assertEquals(false, po.isBasicTaxFree());
+	}
+	
+	@Test
+	public void mustCalculateTax_WhenProductIsTaxFree() {
+		PurchaseOrder po = new PurchaseOrder();
+		
+		po.setTextOrder("1 music book at 10.00");
+		
+		assertEquals(1.50, po.salesTaxCalculator(po));
+		assertEquals(true, po.isMusicProduct());
+		assertEquals(false, po.isImport());
+		assertEquals(true, po.isBasicTaxFree());
 	}
 
 }
